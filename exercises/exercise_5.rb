19cb5359b9d0bd7b2bd91030a8c04af68e64b190
Jeffery <parkjon8@gmail.com>
2022-02-01 06:50:50 +0000

    Complete exercise 6

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+# calculate the total revenue for the entire company
+@total_revenue = Store.sum("annual_revenue")
+puts "Company's total revenue is: #{@total_revenue}"
+
+# average annual revenue for all stores
+@number_of_stores = Store.count
+avg_annual_revenue = @total_revenue / @number_of_stores
+
+puts "Average annual revenue for all stores is: #{avg_annual_revenue}"
+
+# number of stores generating >$1M in annual sales
+puts Store.where("annual_revenue >= 1000000").count
